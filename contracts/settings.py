ad8fc15e89c12a655f1bb350a92cb84cfdc2320b
jibran <jibrankhan205@gmail.com>
2022-03-16 01:12:55 +0500

    forth commit

--- contracts/settings.py
+++ contracts/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'assets/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'assets')]
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
