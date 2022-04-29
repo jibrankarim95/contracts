65dc7a2aff67a9eb2f0422be2987bd08f45875c8
jibran <jibrankhan205@gmail.com>
2022-04-30 03:53:27 +0500

    version 1

--- seao_connect/views.py
+++ seao_connect/views.py
@@ -50,7 +50,7 @@ def seao_upload_data(request):
         password = settings.DATABASES['default']['PASSWORD']
         database_name = settings.DATABASES['default']['NAME']
 
-        database_url = 'postgresql://{user}:{password}@localhost:5432/{database_name}'.format(
+        database_url = 'postgresql://{user}:{password}@ec2-3-230-238-86.compute-1.amazonaws.com:5432/{database_name}'.format(
             user=user,
             password=password,
             database_name=database_name,
@@ -103,11 +103,12 @@ def seao_data_result(request):
         user = settings.DATABASES['default']['USER']
         password = settings.DATABASES['default']['PASSWORD']
         database_name = settings.DATABASES['default']['NAME']
-        database_url = 'postgresql://{user}:{password}@localhost:5432/{database_name}'.format(
+        database_url = 'postgresql://{user}:{password}@ec2-3-230-238-86.compute-1.amazonaws.com:5432/{database_name}'.format(
             user=user,
             password=password,
             database_name=database_name,
         )
+
         engine = create_engine(database_url, echo=False)
         con = engine.raw_connection()
         query_string = ''
