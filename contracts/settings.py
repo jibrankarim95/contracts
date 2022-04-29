6d39d96a206836736da22d819909bb42e059b7e6
jibran <jibrankhan205@gmail.com>
2022-04-29 15:46:36 +0500

    version 1

--- contracts/settings.py
+++ contracts/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "0#qa!#n+w#&t6=r@87ht3x*nav2*##7(fslu(^p&_u@5nq+vo@" #os.environ['SECRET_KEY']
+SECRET_KEY = "0#qa!#n+w#&t6=r@87ht3x*nav2*##7(fslu(^p&_u@5nq+vo@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
